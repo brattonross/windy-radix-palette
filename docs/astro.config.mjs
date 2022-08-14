06f6fb16dcf75f19e437a378747705af9686f319
Ross Bratton <bratton.ross@gmail.com>
2022-08-14 15:06:28 +0100

    docs: styled docs page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [
+    react(),
     tailwind({
       config: {
         applyBaseStyles: false,
