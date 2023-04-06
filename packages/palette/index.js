e162135ebd6a56eec7d7febf76e7fdfcd4e4be06
Gravy59 <gravy@mailsire.com>
2023-04-05 18:35:12 -0600

    allow for alpha value

--- packages/palette/index.js
+++ packages/palette/index.js
@@ -2,62 +2,75 @@ const radix = require("@radix-ui/colors");
 const plugin = require("tailwindcss/plugin");
 
 const windyRadixPalette = plugin.withOptions(
-	({ colors = radix, rootSelector = ":root" } = {}) => {
-		let rootColors = {};
-		let darkModeColors = {};
+  ({ colors = radix, rootSelector = ":root" } = {}) => {
+    let rootColors = {};
+    let darkModeColors = {};
 
-		for (const [colorName, colorObj] of Object.entries(colors)) {
-			const colorMap = colorName.includes("Dark") ? darkModeColors : rootColors;
-			for (const [key, value] of Object.entries(colorObj)) {
-				colorMap[`--${key}`] = value;
-			}
-		}
+    for (const [colorName, colorObj] of Object.entries(radixColors)) {
+      const colorMap = colorName.includes("Dark") ? darkModeColors : rootColors;
+      for (const [key, value] of Object.entries(colorObj)) {
+        if (key.includes("A")) {
+          colorMap[`--${key}`] = value;
+        } else {
+          const [hue, saturation, lightness] = value
+            .match(/\d+/g)
+            ?.map(Number) ?? [0, 0, 0];
+          colorMap[`--${key}`] = `${hue}deg ${saturation}% ${lightness}%`;
+        }
+      }
+    }
 
-		return ({ addBase, config }) => {
-			const [darkMode, className = ".dark"] = [].concat(
-				config("darkMode", "media")
-			);
+    return ({ addBase, config }) => {
+      const [darkMode, className = ".dark"] = [].concat(
+        config("darkMode", "media")
+      );
 
-			if (darkMode === "class") {
-				addBase({
-					[rootSelector]: rootColors,
-					[className]: darkModeColors,
-				});
-			} else {
-				addBase({
-					[rootSelector]: rootColors,
-					"@media (prefers-color-scheme: dark)": {
-						[rootSelector]: darkModeColors,
-					},
-				});
-			}
-		};
-	},
-	({ colors = radix } = {}) => {
-		const themeColors = {};
+      if (darkMode === "class") {
+        addBase({
+          [rootSelector]: rootColors,
+          [className]: darkModeColors,
+        });
+      } else {
+        addBase({
+          [rootSelector]: rootColors,
+          "@media (prefers-color-scheme: dark)": {
+            [rootSelector]: darkModeColors,
+          },
+        });
+      }
+    };
+  },
+  ({ colors = radix } = {}) => {
+    const themeColors = {};
 
-		for (const [colorName, colorObj] of Object.entries(colors)) {
-			if (colorName.includes("Dark")) {
-				continue;
-			}
+    for (const [colorName, colorObj] of Object.entries(radixColors)) {
+      if (colorName.includes("Dark")) {
+        continue;
+      }
 
-			const themeColor = {};
-			for (const key of Object.keys(colorObj)) {
-				const scale = key.replace(colorName, "");
-				themeColor[scale] = `var(--${colorName}${scale})`;
-			}
+      const themeColor = {};
+      for (const key of Object.keys(colorObj)) {
+        const scale = key.replace(colorName, "");
+        if (key.includes("A")) {
+          themeColor[scale] = `var(--${colorName}${scale})`;
+        } else {
+          themeColor[
+            scale
+          ] = `hsl(var(--${colorName}${scale}) / <alpha-value>)`;
+        }
+      }
 
-			themeColors[colorName] = themeColor;
-		}
+      themeColors[colorName] = themeColor;
+    }
 
-		return {
-			theme: {
-				extend: {
-					colors: themeColors,
-				},
-			},
-		};
-	}
+    return {
+      theme: {
+        extend: {
+          colors: themeColors,
+        },
+      },
+    };
+  }
 );
 
 module.exports = windyRadixPalette;
