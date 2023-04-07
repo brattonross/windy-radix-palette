b74fec61d056e4208d6ea19bb2afc259d631fbc3
Gravy59 <gravy@mailsire.com>
2023-04-07 11:17:42 -0600

    feat: allow for alpha value (#16)
    * allow for alpha value
    
    * fix: resolve var names
    
    * fix regex issue
    
    * fix: toRadixVar function use alpha-value
    
    * fix: typography plugin with alpha values
    
    ---------
    
    Co-authored-by: Ross Bratton <bratton.ross@gmail.com>

--- packages/palette/index.js
+++ packages/palette/index.js
@@ -1,6 +1,38 @@
 const radix = require("@radix-ui/colors");
 const plugin = require("tailwindcss/plugin");
 
+/**
+ * Converts a valid CSS HSL string to an object with hue, saturation, and lightness values.
+ * @param {string} hslString - A valid CSS HSL string in the format 'hsl(hue, saturation%, lightness%)'.
+ * @returns {{ hue: number, saturation: number, lightness: number }} An object with hue, saturation, and lightness values.
+ */
+const parseHSL = (hslString) => {
+	/**
+	 * A regular expression that matches a valid CSS HSL string.
+	 * `^hsl\(` matches the beginning of the string and the literal characters "hsl("
+	 * `(\d+(?:\.\d+)?)` captures a number with optional decimal places (i.e., a floating point number) for the hue value.
+	 * `,\s*` matches a comma and any number of whitespace characters (e.g., spaces, tabs) between the hue and saturation values.
+	 * `(\d+(?:\.\d+)?)%` captures a number with optional decimal places followed by a percent sign for the saturation and lightness values.
+	 * `,\s*` matches a comma and any number of whitespace characters between the saturation and lightness values.
+	 * `(\d+(?:\.\d+)?)%` captures a number with optional decimal places followed by a percent sign for the lightness value.
+	 * `\)$` matches the end of the string and the literal character ")".
+	 * @type {RegExp}
+	 */
+	const hslRegex =
+		/^hsl\((\d+(?:\.\d+)?),\s*(\d+(?:\.\d+)?)%,\s*(\d+(?:\.\d+)?)%\)$/;
+
+	const matches = hslString.match(hslRegex);
+	if (!matches) {
+		throw new Error(`Invalid CSS HSL string: ${hslString}`);
+	}
+
+	const hue = parseFloat(matches[1]);
+	const saturation = parseFloat(matches[2]);
+	const lightness = parseFloat(matches[3]);
+
+	return { hue, saturation, lightness };
+};
+
 const windyRadixPalette = plugin.withOptions(
 	({ colors = radix, rootSelector = ":root" } = {}) => {
 		let rootColors = {};
@@ -9,7 +41,12 @@ const windyRadixPalette = plugin.withOptions(
 		for (const [colorName, colorObj] of Object.entries(colors)) {
 			const colorMap = colorName.includes("Dark") ? darkModeColors : rootColors;
 			for (const [key, value] of Object.entries(colorObj)) {
-				colorMap[`--${key}`] = value;
+				if (key.includes("A")) {
+					colorMap[`--${key}`] = value;
+				} else {
+					const { hue, saturation, lightness } = parseHSL(value);
+					colorMap[`--${key}`] = `${hue}deg ${saturation}% ${lightness}%`;
+				}
 			}
 		}
 
@@ -44,7 +81,13 @@ const windyRadixPalette = plugin.withOptions(
 			const themeColor = {};
 			for (const key of Object.keys(colorObj)) {
 				const scale = key.replace(colorName, "");
-				themeColor[scale] = `var(--${colorName}${scale})`;
+				if (key.includes("A")) {
+					themeColor[scale] = `var(--${colorName}${scale})`;
+				} else {
+					themeColor[
+						scale
+					] = `hsl(var(--${colorName}${scale}) / <alpha-value>)`;
+				}
 			}
 
 			themeColors[colorName] = themeColor;
--- packages/typography/index.js
+++ packages/typography/index.js
@@ -9,44 +9,50 @@ module.exports = plugin.withOptions(
 			theme: {
 				extend: {
 					typography: ({ theme }) => {
+						// TODO: Why doesn't Tailwind replace `<alpha-value>` for us?
+						function getThemeColor(color, step) {
+							return theme(`colors.${color}[${step}]`).replace(
+								"<alpha-value>",
+								"1"
+							);
+						}
+
 						const themes = {};
 						for (const color of colors) {
 							themes[color] = {
 								css: {
-									"--tw-prose-body": theme(`colors.${color}[12]`),
-									"--tw-prose-headings": theme(`colors.${color}[12]`),
-									"--tw-prose-lead": theme(`colors.${color}[11]`),
-									"--tw-prose-links": theme(`colors.${color}[12]`),
-									"--tw-prose-bold": theme(`colors.${color}[12]`),
-									"--tw-prose-counters": theme(`colors.${color}[11]`),
-									"--tw-prose-bullets": theme(`colors.${color}[7]`),
-									"--tw-prose-hr": theme(`colors.${color}[6]`),
-									"--tw-prose-quotes": theme(`colors.${color}[12]`),
-									"--tw-prose-quote-borders": theme(`colors.${color}[6]`),
-									"--tw-prose-captions": theme(`colors.${color}[11]`),
-									"--tw-prose-code": theme(`colors.${color}[12]`),
-									"--tw-prose-pre-code": theme(`colors.${color}[12]`),
-									"--tw-prose-pre-bg": theme(`colors.${color}[3]`),
-									"--tw-prose-th-borders": theme(`colors.${color}[7]`),
-									"--tw-prose-td-borders": theme(`colors.${color}[6]`),
-									"--tw-prose-invert-body": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-headings": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-lead": theme(`colors.${color}[11]`),
-									"--tw-prose-invert-links": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-bold": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-counters": theme(`colors.${color}[11]`),
-									"--tw-prose-invert-bullets": theme(`colors.${color}[7]`),
-									"--tw-prose-invert-hr": theme(`colors.${color}[6]`),
-									"--tw-prose-invert-quotes": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-quote-borders": theme(
-										`colors.${color}[6]`
-									),
-									"--tw-prose-invert-captions": theme(`colors.${color}[11]`),
-									"--tw-prose-invert-code": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-pre-code": theme(`colors.${color}[12]`),
-									"--tw-prose-invert-pre-bg": theme(`colors.${color}[3]`),
-									"--tw-prose-invert-th-borders": theme(`colors.${color}[7]`),
-									"--tw-prose-invert-td-borders": theme(`colors.${color}[6]`),
+									"--tw-prose-body": getThemeColor(color, 12),
+									"--tw-prose-headings": getThemeColor(color, 12),
+									"--tw-prose-lead": getThemeColor(color, 11),
+									"--tw-prose-links": getThemeColor(color, 12),
+									"--tw-prose-bold": getThemeColor(color, 12),
+									"--tw-prose-counters": getThemeColor(color, 11),
+									"--tw-prose-bullets": getThemeColor(color, 7),
+									"--tw-prose-hr": getThemeColor(color, 6),
+									"--tw-prose-quotes": getThemeColor(color, 12),
+									"--tw-prose-quote-borders": getThemeColor(color, 6),
+									"--tw-prose-captions": getThemeColor(color, 11),
+									"--tw-prose-code": getThemeColor(color, 12),
+									"--tw-prose-pre-code": getThemeColor(color, 12),
+									"--tw-prose-pre-bg": getThemeColor(color, 3),
+									"--tw-prose-th-borders": getThemeColor(color, 7),
+									"--tw-prose-td-borders": getThemeColor(color, 6),
+									"--tw-prose-invert-body": getThemeColor(color, 12),
+									"--tw-prose-invert-headings": getThemeColor(color, 12),
+									"--tw-prose-invert-lead": getThemeColor(color, 11),
+									"--tw-prose-invert-links": getThemeColor(color, 12),
+									"--tw-prose-invert-bold": getThemeColor(color, 12),
+									"--tw-prose-invert-counters": getThemeColor(color, 11),
+									"--tw-prose-invert-bullets": getThemeColor(color, 7),
+									"--tw-prose-invert-hr": getThemeColor(color, 6),
+									"--tw-prose-invert-quotes": getThemeColor(color, 12),
+									"--tw-prose-invert-quote-borders": getThemeColor(color, 6),
+									"--tw-prose-invert-captions": getThemeColor(color, 11),
+									"--tw-prose-invert-code": getThemeColor(color, 12),
+									"--tw-prose-invert-pre-code": getThemeColor(color, 12),
+									"--tw-prose-invert-pre-bg": getThemeColor(color, 3),
+									"--tw-prose-invert-th-borders": getThemeColor(color, 7),
+									"--tw-prose-invert-td-borders": getThemeColor(color, 6),
 								},
 							};
 						}
