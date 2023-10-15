13e0173d68c0ad30fc77c2a4d28e74c8b247c901
Ross Bratton <bratton.ross@gmail.com>
2023-10-15 01:31:20 +0100

    switch docs to vercel adapter

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import nodejs from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
 	site: "https://windy-radix-palette.vercel.app",
@@ -12,7 +12,5 @@ export default defineConfig({
 		}),
 	],
 	output: "server",
-	adapter: nodejs({
-		mode: "standalone",
-	}),
+	adapter: vercel(),
 });
